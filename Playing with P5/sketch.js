2635305e1e6dee16057903c98503612f1fe973a2
Albert <ayanit1@googlemail.com>
2017-03-29 21:34:18 +0100

    Add fully functional ship

--- Playing with P5/sketch.js
+++ Playing with P5/sketch.js
@@ -9,10 +9,12 @@ function draw() {
   background(0);
   ship.render();
   ship.turn();
+  ship.update();
 }
 
 function keyReleased() {
   ship.setRotation(0);
+  ship.boosing(false);
 }
 
 function keyPressed() {
@@ -20,6 +22,8 @@ function keyPressed() {
     ship.setRotation(0.1);
   } else if (keyCode == LEFT_ARROW) {
     ship.setRotation(-0.1)
+  } else if (keyCode ==UP_ARROW) {
+    ship.boosting(true);
   }
 }
 
@@ -28,11 +32,30 @@ function Ship() {
   this.r = 20;
   this.heading = 0;
   this.rotation = 0,
+  this.vel = createVector(0, 0); 
+  this.isBoosting = false;
   
+  this.boosting = function(b) {
+    this.isBoosting = b;
+  }
+  
+  this.update = function() {
+    if (this.isBoosting) {
+      this.boost();
+    }
+    this.pos.add(this.vel);
+    this.vel.mult(0.99);
+  }
+  
+  this.boost = function() {
+    var force = p5.Vector.fromAngle(this.heading);
+    force.mult(0.1);
+    this.vel.add(force);
+  }
   
   this.render = function() {
     translate(this.pos.x, this.pos.y);
-    rotate(this.heading);
+    rotate(this.heading + PI / 2);
     noFill();
     stroke(255);
     triangle(-this.r, this.r, this.r, this.r, 0, -this.r);
