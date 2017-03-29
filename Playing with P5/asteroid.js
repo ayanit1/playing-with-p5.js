c2ccc45f2070b986d763d5ad8028194f53297329
Albert <ayanit1@googlemail.com>
2017-03-29 22:14:38 +0100

    Add asteroids

--- Playing with P5/asteroid.js
+++ Playing with P5/asteroid.js
@@ -1,13 +1,31 @@
 function Asteroid() {
   this.pos = createVector(random(width), random(height))
-  this.r = 50;
+  this.r = random(15, 50);
+  this.total = floor(random (5, 15))
+  this.offset = [];
+  for (var i = 0; i < this.total; i++){
+    this.offset[i] = random (-15, 5)
+  }
   
   this.render = function(){
     push();
     stroke(255);
     noFill();
     translate(this.pos.x, this.pos.y);
-    ellipse(0, 0, this.r * 2);
+//    ellipse(0, 0, this.r * 2);
+    beginShape();
+    for (var i = 0; i < 10; i++) {
+      var angle = map(i, 0, this.total, 0, TWO_PI);
+      var r = this.r + this.offset[i];
+      var x = r * cos(angle);
+      var y = r * sin(angle);
+      vertex(x, y);
+    }
+    endShape(CLOSE);
+    
+    
+    
+    
     pop();
   }
 } 
--- Playing with P5/sketch.js
+++ Playing with P5/sketch.js
@@ -4,7 +4,9 @@ var asteroids = [];
 function setup() {
 	createCanvas(windowWidth, windowHeight);
   ship = new Ship();
-  asteroids.push(new Asteroid());
+  for (var i = 0; i < 5; i++){
+    asteroids.push(new Asteroid());
+  }
 }
 
 function draw() {
