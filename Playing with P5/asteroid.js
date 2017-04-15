81282b13e8556d3dfccc20365d686e61da6e89c2
Albert <ayanit1@googlemail.com>
2017-04-15 23:06:04 +0100

    Add shoot asteroids feature

--- Playing with P5/asteroid.js
+++ Playing with P5/asteroid.js
@@ -1,10 +1,25 @@
-function Asteroid() {
-  this.pos = createVector(random(width), random(height))
-  this.r = random(15, 50);
+function Asteroid(pos, r) {
+  if (pos){
+    this.pos = pos.copy();
+  } else {
+    this.pos = createVector(random(width), random(height))
+  }
+  
+  if (r) {
+    this.r = r * 0.5
+  } else {
+    this.r = random(15, 50);
+  }
+    
+  this.vel = p5.Vector.random2D();
   this.total = floor(random (5, 15))
   this.offset = [];
   for (var i = 0; i < this.total; i++){
-    this.offset[i] = random (-15, 5)
+    this.offset[i] = random (-this.r * 0.5, this.r)
+  }
+  
+  this.update = function(){
+    this.pos.add(this.vel);
   }
   
   this.render = function(){
@@ -22,10 +37,26 @@ function Asteroid() {
       vertex(x, y);
     }
     endShape(CLOSE);
-    
-    
-    
-    
     pop();
+  }
+  
+  this.breakup = function(){
+    var newA = [];
+    newA[0] = new Asteroid(this.pos, this.r);
+    newA[1] = new Asteroid(this.pos, this.r);
+    return newA;
+  }
+  
+    this.edges = function() {
+    if (this.pos.x > width + this.r) {
+      this.pos.x = -this.r;
+    } else if (this.pos.x < -this.r) {
+      this.pos.x = width + this.r;
+    }
+    if (this.pos.y > height + this.r) {
+      this.pos.y = -this.r;
+    } else if (this.pos.y < -this.r) {
+      this.pos.y = height + this.r;
+    }
   }
 } 
